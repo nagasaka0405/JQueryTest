fec4ef9325011719dfa27d21a598d3ef74f476a7
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-13 18:14:49 +0900

    [fix]thisテスト

--- app.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.box').slideDown(function (){
-        $('.box').css({
-            'background-color': '#0000FF',
-            'width': '100px',
-            'height': '100px'
-        })
-    });
-});
